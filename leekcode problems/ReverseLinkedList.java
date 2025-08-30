0e48f24d30a398a1b49c58a6ef5e34bb3162bfbe
Sunil Singh <sunilsinghrajput192@gmail.com>
2025-08-30 22:18:23 +0530

    Add files via upload

---
+++ leekcode problems/ReverseLinkedList.java
@@ -0,0 +1,43 @@
+
+class ReverseLinkedList {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode nextNode = curr.next; 
+            curr.next = prev;             
+            prev = curr;            
+            curr = nextNode;              
+        }
+        return prev; 
+    }
+
+
+
+    public static void main(String[] args) {
+        
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        // Reverse using Solution class
+        ReverseLinkedList sol = new ReverseLinkedList();
+        ListNode newHead = sol.reverseList(head);
+
+        printList(newHead);
+    }
+
+
+    public static void printList(ListNode head) {
+        ListNode curr = head;
+        while (curr != null) {
+            System.out.print(curr.val + " ");
+            curr = curr.next;
+        }
+        System.out.println();
+    }
+    
+}
