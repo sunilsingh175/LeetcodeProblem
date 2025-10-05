3012b4f8227cb5bc1beb1651714ad12699792d84
Sunil Singh <sunilsinghrajput192@gmail.com>
2025-10-05 22:42:08 +0530

    Add files via upload

---
+++ leekcode problems/SurroundedRegions.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+class SurroundedRegions{
+    public void solve(char[][] board) {
+        if (board == null || board.length == 0) return;
+        int m = board.length, n = board[0].length;
+
+        for (int i = 0; i < m; i++) {
+            if (board[i][0] == 'O') dfs(board, i, 0);
+            if (board[i][n - 1] == 'O') dfs(board, i, n - 1);
+        }
+        for (int j = 0; j < n; j++) {
+            if (board[0][j] == 'O') dfs(board, 0, j);
+            if (board[m - 1][j] == 'O') dfs(board, m - 1, j);
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'O') board[i][j] = 'X';
+                if (board[i][j] == 'T') board[i][j] = 'O';
+            }
+        }
+    }
+
+    private void dfs(char[][] board, int i, int j) {
+        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length || board[i][j] != 'O')
+            return;
+        board[i][j] = 'T';
+        dfs(board, i + 1, j);
+        dfs(board, i - 1, j);
+        dfs(board, i, j + 1);
+        dfs(board, i, j - 1);
+    }
+
+    public static void main(String[] args) {
+        SurroundedRegions sol = new SurroundedRegions();
+        char[][] board = {
+            {'X','X','X','X'},
+            {'X','O','O','X'},
+            {'X','X','O','X'},
+            {'X','O','X','X'}
+        };
+        sol.solve(board);
+
+        for (char[] row : board) {
+            System.out.println(Arrays.toString(row));
+        }
+    }
+}
