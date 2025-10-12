dcf4bd1ad0fa6418b02ca48cce72d63c863200d2
Sunil Singh <sunilsinghrajput192@gmail.com>
2025-10-12 21:43:17 +0530

    Add files via upload

---
+++ leekcode problems/WordLadder.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public class WordLadder {
+
+   
+    public static int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> wordSet = new HashSet<>(wordList);
+        if (!wordSet.contains(endWord)) return 0;
+
+        Queue<String> queue = new LinkedList<>();
+        queue.add(beginWord);
+        int level = 1;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                String word = queue.poll();
+                if (word.equals(endWord)) return level;
+
+                char[] chars = word.toCharArray();
+                for (int j = 0; j < chars.length; j++) {
+                    char originalChar = chars[j];
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        if (c == originalChar) continue;
+                        chars[j] = c;
+                        String newWord = new String(chars);
+                        if (wordSet.contains(newWord)) {
+                            queue.add(newWord);
+                            wordSet.remove(newWord); // mark as visited
+                        }
+                    }
+                    chars[j] = originalChar;
+                }
+            }
+            level++;
+        }
+        return 0;
+    }
+
+    // Main method for testing
+    public static void main(String[] args) {
+        String beginWord = "hit";
+        String endWord = "cog";
+        List<String> wordList = Arrays.asList("hot", "dot", "dog", "lot", "log", "cog");
+
+        int result = ladderLength(beginWord, endWord, wordList);
+        System.out.println(result);
+    }
+}
