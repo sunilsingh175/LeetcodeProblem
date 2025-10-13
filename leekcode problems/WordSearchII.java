e0b6d586db15ce95375b1e213d8f5a7ddb71961b
Sunil Singh <sunilsinghrajput192@gmail.com>
2025-10-13 21:59:47 +0530

    Add files via upload

---
+++ leekcode problems/WordSearchII.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+class WordSearchII {
+    class TrieNode {
+        TrieNode[] children = new TrieNode[26];
+        String word;
+    }
+
+    public List<String> findWords(char[][] board, String[] words) {
+        List<String> result = new ArrayList<>();
+        TrieNode root = buildTrie(words);
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                dfs(board, i, j, root, result);
+            }
+        }
+        return result;
+    }
+
+    private void dfs(char[][] board, int i, int j, TrieNode node, List<String> result) {
+        char c = board[i][j];
+        if (c == '#' || node.children[c - 'a'] == null) return;
+        node = node.children[c - 'a'];
+        if (node.word != null) {
+            result.add(node.word);
+            node.word = null;
+        }
+        board[i][j] = '#';
+        if (i > 0) dfs(board, i - 1, j, node, result);
+        if (j > 0) dfs(board, i, j - 1, node, result);
+        if (i < board.length - 1) dfs(board, i + 1, j, node, result);
+        if (j < board[0].length - 1) dfs(board, i, j + 1, node, result);
+        board[i][j] = c;
+    }
+
+    private TrieNode buildTrie(String[] words) {
+        TrieNode root = new TrieNode();
+        for (String w : words) {
+            TrieNode node = root;
+            for (char c : w.toCharArray()) {
+                if (node.children[c - 'a'] == null)
+                    node.children[c - 'a'] = new TrieNode();
+                node = node.children[c - 'a'];
+            }
+            node.word = w;
+        }
+        return root;
+    }
+
+    public static void main(String[] args) {
+        WordSearchII sol = new WordSearchII();
+        char[][] board = {
+            {'o','a','a','n'},
+            {'e','t','a','e'},
+            {'i','h','k','r'},
+            {'i','f','l','v'}
+        };
+        String[] words = {"oath","pea","eat","rain"};
+        List<String> result = sol.findWords(board, words);
+        System.out.println(result);
+    }
+}
