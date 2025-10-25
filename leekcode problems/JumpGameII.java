683565c9a89f2f76dd17250b45d12e03a4c74438
Sunil Singh <sunilsinghrajput192@gmail.com>
2025-10-25 22:19:36 +0530

    Add files via upload

---
+++ leekcode problems/JumpGameII.java
@@ -0,0 +1,22 @@
+public class JumpGameII {
+    public static void main(String[] args) {
+        int[] nums = {2, 3, 1, 1, 4};
+        System.out.println(jump(nums));
+    }
+
+    public static int jump(int[] nums) {
+        int jumps = 0;
+        int currentEnd = 0;
+        int farthest = 0;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            farthest = Math.max(farthest, i + nums[i]);
+            if (i == currentEnd) {
+                jumps++;
+                currentEnd = farthest;
+            }
+        }
+
+        return jumps;
+    }
+}
