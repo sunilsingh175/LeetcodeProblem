199a7f6169eaa99b26d76890ba7e05bd5e0675ff
Sunil Singh <sunilsinghrajput192@gmail.com>
2025-10-30 21:10:53 +0530

    Add files via upload

---
+++ leekcode problems/ClimbingStairs.java
@@ -0,0 +1,17 @@
+public class ClimbingStairs {
+    public static void main(String[] args) {
+        System.out.println(climbStairs(2));
+        System.out.println(climbStairs(3));
+    }
+
+    public static int climbStairs(int n) {
+        if (n <= 2) return n;
+        int a = 1, b = 2;
+        for (int i = 3; i <= n; i++) {
+            int temp = a + b;
+            a = b;
+            b = temp;
+        }
+        return b;
+    }
+}
