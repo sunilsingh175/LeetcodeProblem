6d59505e7920ad686724e6bc936b0b35fa3b3c36
Sunil Singh <sunilsinghrajput192@gmail.com>
2025-11-02 22:04:52 +0530

    Add files via upload

---
+++ leekcode problems/JumpGame.java
@@ -0,0 +1,19 @@
+public class JumpGame {
+    public static boolean canJump(int[] nums) {
+        int maxReach = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > maxReach) {
+                return false;
+            }
+            maxReach = Math.max(maxReach, i + nums[i]);
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {2, 3, 1, 1, 4};
+        int[] nums2 = {3, 2, 1, 0, 4};
+        System.out.println(canJump(nums1));
+        System.out.println(canJump(nums2));
+    }
+}
