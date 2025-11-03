94400b808d1479f81e9ba8777c2d42d33e318ec8
Sunil Singh <sunilsinghrajput192@gmail.com>
2025-11-03 21:28:24 +0530

    Add files via upload

---
+++ leekcode problems/MaxProductSubArray.java
@@ -0,0 +1,21 @@
+public class MaxProductSubArray {
+    public static int maxProduct(int[] nums) {
+        int max = nums[0], min = nums[0], ans = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < 0) {
+                int temp = max;
+                max = min;
+                min = temp;
+            }
+            max = Math.max(nums[i], max * nums[i]);
+            min = Math.min(nums[i], min * nums[i]);
+            ans = Math.max(ans, max);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxProduct(new int[]{2, 3, -2, 4}));
+        System.out.println(maxProduct(new int[]{-2, 0, -1}));
+    }
+}
