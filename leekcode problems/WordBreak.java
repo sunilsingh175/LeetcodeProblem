4a326943f2ba49a4da66065ae36ee74e04381425
Sunil Singh <sunilsinghrajput192@gmail.com>
2025-11-06 20:23:59 +0530

    Add files via upload

---
+++ leekcode problems/WordBreak.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class WordBreak {
+    public static boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> set = new HashSet<>(wordDict);
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && set.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(wordBreak("leetcode", Arrays.asList("leet", "code")));
+        System.out.println(wordBreak("applepenapple", Arrays.asList("apple", "pen")));
+        System.out.println(wordBreak("catsandog", Arrays.asList("cats", "dog", "sand", "and", "cat")));
+    }
+}
